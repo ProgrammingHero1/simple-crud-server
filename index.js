ab7305be4f76aa45b5d1b699f6091567bdd07858
Programming Hero <programming.hero1@gmail.com>
2023-05-03 10:28:32 -0500

    added update user route

--- index.js
+++ index.js
@@ -37,6 +37,13 @@ async function run() {
         res.send(result);
     })
 
+    app.get('/users/:id', async(req, res) => {
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const user = await userCollection.findOne(query);
+        res.send(user);
+    })
+
     app.post('/users', async(req, res) => {
         const user = req.body;
         console.log('new user', user);
@@ -44,13 +51,32 @@ async function run() {
         res.send(result);
     });
 
+    app.put('/users/:id', async(req, res) =>{
+        const id = req.params.id;
+        const user = req.body;
+        console.log(id, user);
+        
+        const filter = {_id: new ObjectId(id)}
+        const options = {upsert: true}
+        const updatedUser = {
+            $set: {
+                name: user.name,
+                email: user.email
+            }
+        }
+
+        const result = await userCollection.updateOne(filter, updatedUser, options );
+        res.send(result);
+
+    })
+
     app.delete('/users/:id', async(req, res) =>{
         const id = req.params.id;
         console.log('please delete from database', id);
         const query = { _id: new ObjectId(id)}
         
         const result = await userCollection.deleteOne(query);
-        res.send(result)
+        res.send(result);
     })
 
 
